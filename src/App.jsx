e30e88590a730ba884ed28b5b6450288a8fff268
Khuê Minh <mkhue720@gmail.com>
2024-01-18 02:17:45 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useState, useEffect } from 'react';
+import 'boxicons/css/boxicons.min.css';
 import './App.css'
 import Layout from './layout/Layout'
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light');
+  };
+
 
   return (
-    <>
-    <Layout />
-    </>
+    <div className={theme}>
+      <Layout theme={theme} />
+      <div className="theme-toggle ">
+        <button onClick={toggleTheme}>
+          {theme === 'light' 
+          ? <i className='bx bx-moon' ></i>
+          : <i className='bx bx-sun' style={{color:"#f6fc07"}}  ></i>}
+        </button>
+    </div>
+    </div>
   )
 }
 
-export default App
+export default App;
