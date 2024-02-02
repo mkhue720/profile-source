47b82b93f234b8f351e5e2047da5f502d6dedde3
Khuê Minh <mkhue720@gmail.com>
2024-02-03 00:28:32 +0700

    update

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ import dotenv from 'dotenv';
 import authRoute from './Routes/auth.js'
 import userRoute from './Routes/user.js'
 import blogRoute from './Routes/blog.js'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 dotenv.config()
 
 const app = express()
@@ -70,9 +77,23 @@ app.use('/api/v1/blogs', blogRoute)
 //         res.send(html);
 //     }
 // });
-app.get("/*", (req, res) => {
-        res.status(404).send("<h1>404</h1>");
-      });
+// app.get('/app', (req, res) => {
+//         res.send(renderSSR(React.createElement(App, null)));
+//       })
+    
+app.use((error,req,res,next)=>{
+        const statusCode = error.statusCode || 500 ;
+        const message = error.message || 'Internal Server Error';
+        res.status(statusCode).json({
+            success: false,
+            statusCode,
+            message});
+        });
+        
+        app.use(express.static(path.join(__dirname,"client/dist")));
+        app.get("*", (req,res)=>{
+            res.sendFile(path.join(__dirname, "client","dist","index.html"))
+        });
 app.listen(port, () => {
         connectDB();
         console.log('Server is running on port ' + port)
