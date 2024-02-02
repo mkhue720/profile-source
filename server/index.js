65fada61f5cce746b5ec96a7c1a8cd665e8ff890
Khuê Minh <mkhue720@gmail.com>
2024-02-02 22:55:35 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,17 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
+import cors from 'cors'
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-import path from 'path';
-import { renderToString } from 'react-dom/server';
-import { readFileSync } from 'fs';
-import React from 'react';
-import App from '../client/src/App';
-import { StaticRouter } from 'react-router-dom';
-import { ServerStyleSheet } from 'styled-components';
-
+import authRoute from './Routes/auth.js'
+import userRoute from './Routes/user.js'
+import blogRoute from './Routes/blog.js'
 dotenv.config()
+
 const app = express()
 const port = process.env.PORT || 8000
 
@@ -23,13 +19,13 @@ const corsOptions = {
 mongoose.set('strictQuery',false)
 const connectDB = async() => {
         try {
-            await mongoose.connect(process.env.MONGO_URL, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-            console.log('connect db')
+                await mongoose.connect(process.env.MONGO_URL, {
+                        useNewUrlParser: true,
+                        useUnifiedTopology: true,
+                })
+                console.log('connect db')
         } catch (err) {
-            console.log('connect db failed')
+                console.log('connect db failed')
         }
 }
 
@@ -41,40 +37,42 @@ app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/users', userRoute)
 app.use('/api/v1/blogs', blogRoute)
 
-app.get('*', (req, res) => {
-    const context = {};
-    const sheet = new ServerStyleSheet();
+// app.get('*', (req, res) => {
+//     const context = {};
+//     const sheet = new ServerStyleSheet();
 
-    const appHtml = renderToString(
-        sheet.collectStyles(
-            <StaticRouter location={req.url} context={context}>
-                <App />
-            </StaticRouter>
-        )
-    );
+//     const appHtml = renderToString(
+//         sheet.collectStyles(
+//             <StaticRouter location={req.url} context={context}>
+//                 <App />
+//             </StaticRouter>
+//         )
+//     );
 
-    const styleTags = sheet.getStyleTags(); // or sheet.getStyleElement();
+//     const styleTags = sheet.getStyleTags(); // or sheet.getStyleElement();
     
-    if (context.url) {
-        res.redirect(301, context.url);
-    } else {
-        const html = `
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    ${styleTags}
-                </head>
-                <body>
-                    <div id="root">${appHtml}</div>
-                    <script src="/bundle.js"></script>
-                </body>
-            </html>
-        `;
+//     if (context.url) {
+//         res(301, context.url);
+//     } else {
+//         const html = `
+//             <!DOCTYPE html>
+//             <html>
+//                 <head>
+//                     ${styleTags}
+//                 </head>
+//                 <body>
+//                     <div id="root">${appHtml}</div>
+//                     <script src="/bundle.js"></script>
+//                 </body>
+//             </html>
+//         `;
 
-        res.send(html);
-    }
-});
-
+//         res.send(html);
+//     }
+// });
+app.get("/*", (req, res) => {
+        res.status(404).send("<h1>404</h1>");
+      });
 app.listen(port, () => {
         connectDB();
         console.log('Server is running on port ' + port)
