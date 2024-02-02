db69d84a9ac8bba2bc63a0107b89e407e901be62
Khuê Minh <mkhue720@gmail.com>
2024-02-03 01:48:28 +0700

    update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,2 @@
 export const BASE_URL = 'https://profile-source.onrender.com/api/v1'
-<<<<<<< HEAD
 export const token = localStorage.getItem('token')
-=======
-export const token = localStorage.getItem('token')
->>>>>>> f6fec725c1a7e3cf4799018fec9a0aa5771228dd
